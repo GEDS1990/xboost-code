2ee7077c9a367c396e40db7d22764b4011a3cd70
yangxq729 <yangxq729@163.com>
2017-11-24 00:50:19 +0800

    Add ScenariosMapper.java by yxq

---
+++ xboost/com.xboost.business/src/main/java/com/xboost/mapper/ScenariosMapper.java
@@ -0,0 +1,42 @@
+package com.xboost.mapper;
+
+import com.xboost.pojo.Scenarios;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/11/5 0005.
+ */
+public interface ScenariosMapper {
+
+    //  添加场景信息  @param scenario
+    void save(Scenarios scenario);
+
+
+     // 查询所有场景信息  @return
+    List<Scenarios> findAll();
+
+    Long findAllCount();
+
+    // 根据参数查询场景信息  @param param @return
+    List<Scenarios> findByParam(Map<String, Object> param);
+    Long findCountByParam(Map<String, Object> param);
+
+    /**
+     * 根据ID查询场景信息
+     * @param id
+     * @return
+     */
+    Scenarios findById(Integer id);
+
+
+    // 根据id编辑场景信息  @param scenario
+    void edit(Scenarios scenario);
+
+
+    //根据id删除场景信息   * @param id
+    void delById(Integer id);
+
+}
+
