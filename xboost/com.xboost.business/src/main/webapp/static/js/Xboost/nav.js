06e64f9eb1a83ed68250864420b36e53d5419f6f
zkj <739300585@qq.com>
2017-11-25 14:26:51 +0800

    mod by zkj @20171125

--- xboost/com.xboost.business/src/main/webapp/static/js/Xboost/nav.js
+++ xboost/com.xboost.business/src/main/webapp/static/js/Xboost/nav.js
@@ -6,6 +6,7 @@ $(document).ready(function  () {
 		console.log(11)
 		$(this).parents("#scen-name").next("#scen-class").remove();
 		$(this).parents("#scen-name").remove();
+		window.location.href = "/MyScenarios";
 		return false;
 	});
 });	
