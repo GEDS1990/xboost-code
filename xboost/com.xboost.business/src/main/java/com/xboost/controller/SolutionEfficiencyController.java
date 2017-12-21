9f7ecb0fbf04395c4fd53a39e636bda8f45b2374
yangxq729 <yangxq729@163.com>
2017-12-22 01:02:57 +0800

    Mod by yxq @20171221

--- xboost/com.xboost.business/src/main/java/com/xboost/controller/SolutionEfficiencyController.java
+++ xboost/com.xboost.business/src/main/java/com/xboost/controller/SolutionEfficiencyController.java
@@ -1,17 +1,70 @@
 package com.xboost.controller;
 
+import com.google.common.collect.Maps;
+import com.xboost.pojo.Route;
+import com.xboost.service.SolutionCostService;
+import com.xboost.service.SolutionEfficiencyService;
+import com.xboost.service.SolutionRouteService;
+import com.xboost.service.DemandInfoService;
+import com.xboost.util.ShiroUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 
 @Controller
 @RequestMapping("/efficiency")
 public class SolutionEfficiencyController {
+    @Inject
+    SolutionRouteService solutionRouteService;
+    @Inject
+    DemandInfoService demandInfoService;
+    @Inject
+    SolutionEfficiencyService solutionEfficiencyService;
+
     @RequestMapping(method = RequestMethod.GET)
     public String list() {
         return "results/efficiency";
     }
 
-    
+    //发出票数
+    @RequestMapping(value = "/efficiency.json",method = RequestMethod.GET,produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public Map<String,Object> load(HttpServletRequest request) {
+        String scenariosId = ShiroUtil.getOpenScenariosId();
+
+        int periodTime = 10;
+
+        String maxmix = demandInfoService.findMinMax(ShiroUtil.getOpenScenariosId());
+        int min = Integer.parseInt(maxmix.split("-")[0]);
+        int max = Integer.parseInt(maxmix.split("-")[1]);
+
+        List<Route> routeList = solutionRouteService.findAllRoute(scenariosId);
+        List<String> siteList = solutionEfficiencyService.findAllSite(scenariosId);
+
+        Map<String,Object> result = Maps.newHashMap();
+        Map<String,Object> param = Maps.newHashMap();
+        Integer sbVol;
+
+        for(int i=0;i<siteList.size();i++){
+            String site = siteList.get(i);
+            for(int j=0;j<(max-min)/periodTime;j++)
+            {
+                param.put("scenariosId",scenariosId);
+                param.put("curLoc",site);
+                param.put("min",min);
+                param.put("periodTime",periodTime);
+                sbVol = solutionEfficiencyService.findSbVol(param);
+                result.put(String.valueOf(min+(periodTime*j))+"-"+String.valueOf(min+(periodTime*(j+1))),site+"-"+sbVol);
+            }
+        }
+
+        return result;
+    }
 }
---
+++ xboost/com.xboost.business/src/main/java/com/xboost/service/SolutionEfficiencyService.java
@@ -0,0 +1,54 @@
+package com.xboost.service;
+
+import com.xboost.mapper.SolutionRouteMapper;
+import com.xboost.mapper.SolutionEfficiencyMapper;
+import com.xboost.pojo.Route;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/11/5 0005.
+ */
+
+@Named
+@Transactional
+public class SolutionEfficiencyService {
+    private static Logger logger = LoggerFactory.getLogger(SolutionEfficiencyService.class);
+
+    @Inject
+    private SolutionRouteMapper solutionRouteMapper;
+    @Inject
+    private SolutionEfficiencyMapper solutionEfficiencyMapper;
+
+
+    /**
+     * 根据场景id查询所有路线信息
+     * param
+     * @return
+     */
+    public List<Route> findAllRoute(String scenariosId) {
+        return solutionRouteMapper.findAll(scenariosId);
+    }
+
+    /**
+     * 查询所有网点
+     * param
+     * @return
+     */
+    public List<String> findAllSite(String scenariosId) {
+        return solutionEfficiencyMapper.findAllSite(scenariosId);
+    }
+
+    public Integer findSbVol(Map<String,Object> param){
+        return solutionEfficiencyMapper.findSbVol(param);
+    }
+
+
+}
