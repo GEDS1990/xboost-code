2c42b36ac623bfc2edf10df1e290748525c9a658
GEDS1990 <1161865398@qq.com>
2017-12-22 07:53:57 +0800

    mod by geds@20171222

---
+++ xboost/com.xboost.business/src/main/java/com/xboost/mapper/SolutionEfficiencyMapper.java
@@ -0,0 +1,18 @@
+package com.xboost.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/11/5 0005.
+ */
+public interface SolutionEfficiencyMapper {
+
+    List<Map<String,Object>> findByCar(Map<String, Object> param);
+
+    List<String> findAllSite(String scenariosId);
+
+    Integer findSbVol(Map<String, Object> param);
+
+}
+
