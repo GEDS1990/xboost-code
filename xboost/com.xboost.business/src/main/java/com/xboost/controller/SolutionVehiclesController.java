b9d866776b1483fbd5f3d6d4eace08bf6397bdec
yangxq729 <yangxq729@163.com>
2018-01-11 23:32:09 +0800

    mod @20170111 by yxq

--- xboost/com.xboost.business/src/main/java/com/xboost/controller/SolutionVehiclesController.java
+++ xboost/com.xboost.business/src/main/java/com/xboost/controller/SolutionVehiclesController.java
@@ -73,7 +73,7 @@ public class SolutionVehiclesController {
         param.put("start",start);
         param.put("length",length);
         if(StringUtils.isNotEmpty(searchValue)) {
-            param.put("keyword", Strings.toUTF8(searchValue));
+            param.put("keyword", searchValue);
         }
         param.put("orderColumn",orderColumnName);
         param.put("orderType",orderType);
