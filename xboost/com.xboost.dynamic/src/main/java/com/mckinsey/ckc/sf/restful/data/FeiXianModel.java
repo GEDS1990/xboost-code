ba322719052d0e58f6a009cc0bb9e56514235c34
yangxq729 <yangxq729@163.com>
2018-02-10 16:57:40 +0800

    mod by yxq @20180210

---
+++ xboost/com.xboost.dynamic/src/main/java/com/mckinsey/ckc/sf/restful/data/FeiXianModel.java
@@ -0,0 +1,22 @@
+package com.mckinsey.ckc.sf.restful.data;
+
+public class FeiXianModel {
+	private String x;
+	private String y;
+	public String getX() {
+		return x;
+	}
+	public void setX(String x) {
+		this.x = x;
+	}
+	public String getY() {
+		return y;
+	}
+	public void setY(String y) {
+		this.y = y;
+	}
+
+	
+	
+
+}
---
+++ xboost/com.xboost.dynamic/src/main/java/com/mckinsey/ckc/sf/restful/data/XYModel.java
@@ -0,0 +1,21 @@
+package com.mckinsey.ckc.sf.restful.data;
+
+public class XYModel {
+	private String x;
+	private double y;
+	public String getX() {
+		return x;
+	}
+	public void setX(String x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	
+
+}
